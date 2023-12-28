f25534d621f7656bf54ad5e5a74b966ce1c9c4cc
BelowNeutral <tran.christopherd1@gmail.com>
2023-12-28 16:21:33 -0600

    on step 4. currently stuck + debugging code (DO NOT PUSH CODE)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getComputerChoice() {
     const rockPaperScissors = [
-        "Rock",
-        "Paper",
-        "Scissors"
+        "rock",
+        "paper",
+        "scissors"
     ];
 
     const randomChoice = rockPaperScissors[Math.floor(Math.random()* rockPaperScissors.length)];
@@ -10,3 +10,29 @@ function getComputerChoice() {
 }
 
 // getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    // let userInput = prompt("Rock, Paper, Scissors...Shoot!");
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
+    // userInput.toLowerCase();
+    // alert("You win! Paper beats Rock!");
+    // alert("You win! Scissors beats Paper!");
+    // alert("You win! Rock beats Scissors!");
+    // alert("You tied! Rock and Rock!");
+    // alert("You tied! Paper and Paper!");
+    // alert("You tied! Scissors and Scissors!");
+    
+    
+    
+    // alert("You lose! Paper beats Rock!");
+    // alert("You lose! Scissors beat Paper!");
+    // alert("You lose! Rock beats Scissors!");
+
+
+startRound();
