388c69fd7f2c5db84579b2af2a4d9f78b362d51b
BelowNeutral <tran.christopherd1@gmail.com>
2023-12-28 18:59:29 -0600

    completed rock paper scissors game in console

--- script.js
+++ script.js
@@ -7,32 +7,48 @@ function getComputerChoice() {
 
     const randomChoice = rockPaperScissors[Math.floor(Math.random()* rockPaperScissors.length)];
     console.log(randomChoice);
+
+    return randomChoice;
 }
 
-// getComputerChoice();
 
-function playRound(playerSelection, computerSelection) {
-    // let userInput = prompt("Rock, Paper, Scissors...Shoot!");
+function game() {
+// The playRound function below was created with the help of ChatGPT!
+    function playRound(playerSelection, computerSelection) {
+        playerSelection = prompt("Rock, Paper, Scissors...Shoot!").toLowerCase();
+
+        computerSelection = getComputerChoice();
+
+        console.log("Computer choice: " + computerSelection);
+
+        // The if-statements below were written by me!
+        if (playerSelection === computerSelection) {
+            alert("It's a tie! Play again.");
+        } else if (playerSelection === "rock" && computerSelection === "paper") {
+            alert("You lost! Paper beats Rock!");
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            alert("You lost! Scissors beat paper!");
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            alert("You lost! Rock beats scissors!");
+        } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            alert("You win! Rock beats scissors!");
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            alert("You win! Paper beats Rock!");
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            alert("You win! Scissors beat paper!");
+        } else {
+            alert("You must type something!");
+        }
+        console.log("Player choice: " + playerSelection);
+        // The if-statements above were written by me!
+    }
+    playRound();
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// The playRound function above was created with the help of ChatGPT!
 
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
 
-    // userInput.toLowerCase();
-    // alert("You win! Paper beats Rock!");
-    // alert("You win! Scissors beats Paper!");
-    // alert("You win! Rock beats Scissors!");
-    // alert("You tied! Rock and Rock!");
-    // alert("You tied! Paper and Paper!");
-    // alert("You tied! Scissors and Scissors!");
-    
-    
-    
-    // alert("You lose! Paper beats Rock!");
-    // alert("You lose! Scissors beat Paper!");
-    // alert("You lose! Rock beats Scissors!");
-
-
-startRound();
+game();
